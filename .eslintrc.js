4a68d7aa16963c3fd3bb12e58205068d17844471
Adrien RUSSO <adrien.russo@gmail.com>
2019-05-30 15:44:58 +0200

    type filter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es6': true,
-    'node': true,
-  },
-  'extends': 'google',
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly',
-  },
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
     },
-    'ecmaVersion': 2018,
-    'sourceType': 'module',
-  },
-  'plugins': [
-    'react',
-  ],
-  'rules': {
-  },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
 };
--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { useContext } from "react";
 import { RoomContext } from "../context";
-import Title from "../components/Title";
+import Title from "./Title";
 
 // get all unique values
 const getUnique = (items, value) => {
   return [...new Set(items.map(item => item[value]))];
 };
 
-
-
-export default function RoomFilter(rooms) {
+export default function RoomFilter({ rooms }) {
   const context = useContext(RoomContext);
   const {
     handleChange,
@@ -26,37 +24,34 @@ export default function RoomFilter(rooms) {
   } = context;
   // get unique types
   let types = getUnique(rooms, "type");
-
-  //add all
+  // add all
   types = ["all", ...types];
+  // map to jsx
+  types = types.map((item, index) => (
+    <option key={index} value={item}>
+      {item}
+    </option>
+  ));
 
-  // map to JSX
-  types = types.map((item, index) => {
-    return (
-      <option value={item} key={index}>
-        {item}
-      </option>
-    );
-  });
   return (
     <>
       <section className="filter-container">
         <Title title="search rooms" />
         <form className="filter-form">
-          {/* select type */}
+          {/* {select type} */}
           <div className="form-group">
             <label htmlFor="type">room type</label>
             <select
               name="type"
               id="type"
-              value={type}
-              className="form-control"
               onChange={handleChange}
+              className="form-control"
+              value={type}
             >
               {types}
             </select>
           </div>
-          {/* end of select type */}
+          {/* end select type */}
         </form>
       </section>
     </>
